d1aade8a9434d7060530a0fcccab91f9d59cf1c2
anshanto <asikurjamann@gmail.com>
2022-06-12 01:56:57 +0600

    withdraw amount added

--- js/banking.js
+++ js/banking.js
@@ -21,3 +21,29 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     // clear deposit input field
     depositInput.value = '';
 })
+
+// handle withdraw button event
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    // get the amount withdraw
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+
+    // get withdraw total amount
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawAmountText = withdrawTotal.innerText;
+    const previousWithdrawAmount = parseFloat(previousWithdrawAmountText);
+    const newWithdrawAmountTotal = newWithdrawAmount + previousWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawAmountTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
+
+    // clear withdraw input field
+    withdrawInput.value = '';
+})
